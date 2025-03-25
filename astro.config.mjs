ae6cea29982b6dd1dcc22d8f394c0b6cb3863510
German <BinderGerman>
2025-03-25 17:01:09 -0300

    Initial Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,31 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sanity from '@sanity/astro';
+import react from '@astrojs/react';
+
+/* 
+import { loadEnv } from "vite";
+const { PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_DATASET } = loadEnv(
+  process.env.NODE_ENV,
+  process.cwd(),
+  "",
+); 
+*/
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    sanity({
+      projectId: '9fbd0yax',
+      dataset: 'production',
+      useCdn: false,
+      apiVersion: "2025-03-19",
+      studioBasePath: '/studio',
+      stega: {
+        studioUrl: "/studio",
+      },
+    }), 
+    react()
+  ]
+});
